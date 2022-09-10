fd8fb809e9683be80beaeb2c0aec6fa01dcfb08f
Tarxos <ftarxos@gmail.com>
2022-09-10 15:58:30 +0300

    implement compile behavior in script service

--- WCF/PumpService/Scripting/ScriptService.cs
+++ WCF/PumpService/Scripting/ScriptService.cs
@@ -1,4 +1,10 @@
-﻿using PumpService.App_Data;
+﻿using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Microsoft.CSharp;
+using PumpService.App_Data;
 
 namespace PumpService.Scripting
 {
@@ -8,6 +14,8 @@ namespace PumpService.Scripting
         private readonly IScriptConfiguration _configuration;
         private readonly IStatistics _statistics;
 
+        private CompilerResults _compilerResults;
+
         public ScriptService(IPumpServiceCallback callback, IScriptConfiguration configuration)
         {
             _callback = callback;
@@ -15,10 +23,52 @@ namespace PumpService.Scripting
             _statistics = new Statistics();
         }
 
-        public bool Compile() => false;
+        public bool Compile()
+        {
+            if (!File.Exists(_configuration.FileName)) return false;
+
+            try
+            {
+                CompilerParameters compilerParameters = new CompilerParameters
+                {
+                    GenerateInMemory = true
+                };
+                compilerParameters.ReferencedAssemblies.AddRange(new[]
+                {"System.dll","System.Core.dll","System.Data.dll", "Microsoft.CSharp.dll", Assembly.GetExecutingAssembly().Location});
+
+                CSharpCodeProvider provider = new CSharpCodeProvider();
+                var fileData = LoadFile(_configuration.FileName);
+                _compilerResults = provider.CompileAssemblyFromSource(compilerParameters, fileData);
+                
+                return !_compilerResults.Errors.HasErrors;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
 
         public void Run(int count)
         {
+        }
+
+        private static string LoadFile(string fileName)
+        {
+            FileStream fileStream = new FileStream(fileName, FileMode.Open);
+            try
+            {
+                int length = (int)fileStream.Length;
+                var buffer = new byte[length];
+                int count;
+                int sum = 0;
+                while ((count = fileStream.Read(buffer, sum, length - sum)) > 0)
+                    sum += count;
+                return Encoding.UTF8.GetString(buffer);
+            }
+            finally
+            {
+                fileStream.Close();
+            }
         }
     }
 }
