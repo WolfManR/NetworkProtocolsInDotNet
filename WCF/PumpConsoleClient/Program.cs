939cea975e779efca26e6cf858ef96f71bd248da
Tarxos <ftarxos@gmail.com>
2022-09-10 16:36:02 +0300

    add sample of using pump service in console client

--- WCF/PumpConsoleClient/Program.cs
+++ WCF/PumpConsoleClient/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace PumpConsoleClient
+﻿using System;
+using System.ServiceModel;
+
+using PumpConsoleClient.PumpServiceReference;
+
+namespace PumpConsoleClient
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            InstanceContext instanceContext = new InstanceContext(new CallbackHandler());
+            using (PumpServiceClient client = new PumpServiceClient(instanceContext))
+            {
+                client.UpdateAndCompileScript(@"C:\scripts\Sample.script");
+                client.RunScript();
+
+                Console.WriteLine("Please, Enter to exit ...");
+                Console.ReadKey(true);
+                client.Close();
+            }
         }
     }
 }
