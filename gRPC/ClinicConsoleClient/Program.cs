d53e42065323d1ba118198e6dbebd68ae45781e0
Tarxos <ftarxos@gmail.com>
2022-09-15 03:03:00 +0300

    add console client sample project

---
+++ gRPC/ClinicConsoleClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using static ClinicServiceProtos.ClinicClientService;
+
+AppContext.SetSwitch(
+    "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+var channel = GrpcChannel.ForAddress("http://localhost:5076");
+
+
+ClinicClientServiceClient client = new(channel);
+
+var createClientResponse = client.CreateClient(new()
+{
+    Document = "PASS123",
+    FirstName = "Василий",
+    Surname = "Пупкин",
+    Patronymic = "Картошкович"
+});
+
+Console.WriteLine($"Client ({createClientResponse.ClientId}) created successfully.");
+
+var getClientsResponse = client.GetClients(new());
+
+Console.WriteLine("Clients:");
+Console.WriteLine("========\n");
+foreach (var clientObj in getClientsResponse.Clients)
+{
+    Console.WriteLine($"{clientObj.Document} >> {clientObj.Surname} {clientObj.FirstName}");
+}
+
+Console.ReadKey();
