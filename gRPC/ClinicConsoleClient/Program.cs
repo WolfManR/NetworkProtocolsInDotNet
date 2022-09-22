f0680a2cccc971e483ce4426546a217ef873a5da
Tarxos <ftarxos@gmail.com>
2022-09-22 03:43:41 +0300

    extend console client sample with gRPC authorization

--- gRPC/ClinicConsoleClient/Program.cs
+++ gRPC/ClinicConsoleClient/Program.cs
@@ -1,12 +1,46 @@
-﻿using Grpc.Net.Client;
+﻿using ClinicServiceProtos;
+using Grpc.Core;
+using Grpc.Net.Client;
+
+using static ClinicServiceProtos.AuthenticateService;
 using static ClinicServiceProtos.ClinicClientService;
 
 AppContext.SetSwitch(
     "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
 var channel = GrpcChannel.ForAddress("http://localhost:5076");
 
+AuthenticateServiceClient authenticateServiceClient = new AuthenticateServiceClient(channel);
 
-ClinicClientServiceClient client = new(channel);
+
+var authenticationResponse = authenticateServiceClient.Login(new AuthenticationRequest
+{
+    UserName = "sample@gmail.com",
+    Password = "12345"
+});
+
+if (authenticationResponse.Status != 0)
+{
+    Console.WriteLine("Authentication error.");
+    Console.ReadKey();
+    return;
+}
+
+
+Console.WriteLine($"Session token: {authenticationResponse.SessionContext.SessionToken}");
+
+var callCredentials = CallCredentials.FromInterceptor((c, m) =>
+{
+    m.Add("Authorization",
+        $"Bearer {authenticationResponse.SessionContext.SessionToken}");
+    return Task.CompletedTask;
+});
+
+var protectedChannel = GrpcChannel.ForAddress("https://localhost:7076", new GrpcChannelOptions
+{
+    Credentials = ChannelCredentials.Create(new SslCredentials(), callCredentials)
+});
+
+ClinicClientServiceClient client = new(protectedChannel);
 
 var createClientResponse = client.CreateClient(new()
 {
