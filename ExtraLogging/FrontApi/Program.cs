b5b94e7bf6b207fb059f40332d16ede071ae0b23
Tarxos <ftarxos@gmail.com>
2022-09-24 18:04:14 +0300

    inject Root Api open api http client

--- ExtraLogging/FrontApi/Program.cs
+++ ExtraLogging/FrontApi/Program.cs
@@ -1,9 +1,15 @@
+using FrontApi.Services;
+
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient<RootApiClient>();
 
 var app = builder.Build();
 
@@ -14,28 +20,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
+app.MapGet(
+        "/weatherforecast",
+        async ([FromServices] RootApiClient client) => Results.Ok(await client.GetWeather()))
 .WithName("GetWeatherForecast");
 
 app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
