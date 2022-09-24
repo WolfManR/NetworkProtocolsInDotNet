ad6061890981c06388626d95b6679cacd3f26ed8
Tarxos <ftarxos@gmail.com>
2022-09-24 17:43:23 +0300

    add drop coin for throw exception on root api project

--- ExtraLogging/RootApi/Program.cs
+++ ExtraLogging/RootApi/Program.cs
@@ -21,15 +21,20 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    var dropCoin = Random.Shared.Next(0, 2) == 0;
+
+    if (dropCoin)
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            (
+                DateTime.Now.AddDays(index),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+    }
+
+    throw new Exception("Internal Server Error");
 })
 .WithName("GetWeatherForecast");
 
